9fb5106c42ed3c34cf9111404e848b31836a7e10
LuizCelli <luizcelli@live.com>
2025-06-27 14:05:49 -0300

    Create 1014.c

---
+++ 1014.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   int distancia, combustivel, x;
+   
+   sacnf("%d %d", &distancia, &combustivel);
+
+  x = ( distancia / combustivel );
+
+  printf ("
+   
+   
+    return 0;
+}
