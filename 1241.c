0047c25076b5c549e0a111b64767d8c4a3b923db
LuizCelli <luizcelli@live.com>
2025-07-14 12:39:17 -0300

    Create 1241.c

---
+++ 1241.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int N;
+    char A[1001], B[1001];
+
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%s %s", A, B);
+
+        int lenA = strlen(A);
+        int lenB = strlen(B);
+
+        if (lenB > lenA) {
+            printf("nao encaixa\n");
+        } else {
+            if (strcmp(A + (lenA - lenB), B) == 0) {
+                printf("encaixa\n");
+            } else {
+                printf("nao encaixa\n");
+            }
+        }
+    }
+
+    return 0;
+}
